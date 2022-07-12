0faddf75d1bce4417b8db86113a28204f9af7921
ali zeinali <ali.zeinali70@gmail.com>
2022-07-12 14:39:58 +0430

    working...

--- RazorPitzzeria/Pages/Checkout/Checkout.cshtml.cs
+++ RazorPitzzeria/Pages/Checkout/Checkout.cshtml.cs
@@ -3,10 +3,22 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorPitzzeria.Pages.Checkout
 {
+    [BindProperties(SupportsGet =true)]
     public class CheckoutModel : PageModel
-    {
+    {        
+        public string PizzaName { get; set; }
+        public float PizzaPrice { get; set; }
+        public string ImageTitle { get; set; }
         public void OnGet()
         {
+            if (string.IsNullOrEmpty(PizzaName))
+            {
+                PizzaName = "Custom";
+            }
+            if (string.IsNullOrEmpty(ImageTitle))
+            {
+                ImageTitle = "pizza-man";
+            }
         }
     }
 }
